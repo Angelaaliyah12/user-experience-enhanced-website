001d3617a2c259eab58b7e44119d4c762f78e69d
Angelaaliyah12 <angel.blom@hva.nl>
2026-05-07 22:18:57 +0200

    #3 alle code overgebracht

---
+++ docs/ui-states.md
@@ -0,0 +1,144 @@
+# Interactive functionality
+
+## UI states voor POST interactie
+
+Als je een interactie ontwerpt moet je voor de gebruiker feedback ontwerpen. Je weet dat je met feedback en feedforward ervoor kan zorgen dat gebruikers weten wat ze moeten doen. Dit doe je o.a. met de juiste labels, teksten en button states.  
+
+Omdat we deze sprint met het POSTen van data werken, wordt de _state_ van je website belangrijk. 
+De _state_ geeft aan of er al informatie is opgeslagen of nog niet.  
+Een goed voorbeedld hiervan is de winkelwagen van bol.com. 
+
+![shopping card](shoppingcart.png)
+
+Als een product is toegevoegd kan je dat aan het winkelwagentje zien: 
+
+![shopping card full](shoppingcart-full.png)
+
+
+### Aanpak
+
+Vandaag gaan we eerst onderzoeken wat de UI-stack is. 
+Daarna ga je het toepassen op je eigen opdracht. 
+
+Ook gaan we je vorderingen bespreken om te kijken waar je hulp bij nodig hebt.
+Hoe ver ben je met de opzet van je opdracht en de POST interactie? Schrijf op het whiteboard je naam en hoe ver je bent met jouw opdracht: 1,2,3,4,5,6:
+
+1. Repo met Server.js, Get routes en views opgezet in je nieuwe repo
+2. POST Interactie bedenken en schetsen
+3. Interactie ontwerpen met een Wireflow en breakdown
+4. Bouw de functionaliteit in HTML en server-side: Routes
+5. Bouw de functionaliteit in HTML en server-side: Formulier maken
+6. Bouw de functionaliteit in HTML en server-side: Opslaan in Directus
+7. ...
+
+
+
+
+## UI-Stack onderzoeken
+
+Voor het ontwerpen van states met dynamische data gebruiken we de UI-Stack. 
+In het artikel How to Fix a Bad Interface staat:
+
+> Every screen you interact with in a digital product has multiple personalities.
+> 
+> [How to fix a bad user interface](https://www.scotthurff.com/posts/why-your-user-interface-is-awkward-youre-ignoring-the-ui-stack/)
+
+Voor elk scherm waar een gebruiker iets mee doet moet je verschillende states tonen. 
+Er wordt bijvoorbeeld data geladen, of er kan iets mis gaan. 
+Dan heeft de gebruiker feedback nodig die duidelijk maakt wat er gebeurt. 
+Hiervoor heeft een scherm soms wel 4-6 states nodig. 
+
+1. Loading State
+2. Partial State
+3. Error State
+4. Ideal state
+
+en 
+
+5. Empty State, als er nog niets is gepost
+6. Succes State, als een post is gelukt
+
+
+![UI-stack](ui-stack.jpg) *verschillende states van dezelfde pagina.*
+
+
+
+### Opdracht
+
+Onderzoek met je tafel de states van de UI-Stack:
+
+👉 Schrijf de UI-Stack states op het bord
+
+👉 Zoek voor elke state een goed voorbeeld, heb je ook voorbeelden waar het niet goed gaat?
+
+👉 Schrijf kort op waar ze voor gebruikt worden
+
+👉 Bespreek met je tafel welke UI-stack states je kan gebruiken voor jouw `post` interactie
+
+
+
+## UI-Stack ontwerpen en bouwen
+
+Als het goed is hebt je al een wireflow van jouw interactie. 
+Eigenlijk is dat al de *ideal* state. 
+Vandaag ga je je wireflow uitbreiden met andere states van de UI-Stack.
+
+
+### Wireflow/Screenflow uitbreiden met de UI-Stack
+
+Bespreek je Wireflow over je POST funcionaliteit met je buur en bedenk welke states van de UI-Stack je nodig hebt. 
+Wat laat je bijvoorbeeld zien als er nog geen berichten zijn gepost? 
+Wat zou er mis kunnen gaan met posten en wat voor feedback geef je dan aan de gebruiker? 
+En wat ziet een gebruiker als de POST goed gaat?
+
+Voeg de states toe in Figma en geef ze een duidelijke titel en korte uitleg. 
+Omdat dit states zijn van dezelfde pagina teken je ze niet in de flow van de interactie. 
+Dat ziet er bijvoorbeeld zo uit: 
+
+![UI-stack Wireflow](UI-stack-wireflow.png) 
+
+
+
+<!--
+👉 Breid je wireflow uit met elke UI state
+
+Welke states kunnen ze nu gaan maken? 
+Empty state
+Partial state?
+Ideal state
+
+Een lijst kan gevuld zijn
+Een lijst kan nog leeg zijn, er zit nog niet in
+Een lijst kan gedeeltelijk vol zijn, maar nog niet volledig, iemand moet nog iets doen? 
+
+Group aanmaken maar er zijn nog geen users...
+Lijst met likes op een foto maar je foto is nog niet gelikt ...
+
+
+-->
+
+
+
+
+### UI-Stack states bouwen in Liquid
+
+Omdat we server-side pagina's aan het bouwen zijn, gaan we beginnen met het bouwen van een empty-state. 
+De loading-state heb je pas nodig als je de POST client-side gaat maken in sprint 10.
+
+Om de empty-state te kunnen tonen in Liquid zul je gebruik moeten maken van _if/else statements_. 
+Welke HTML moet gerenderd worden als er geen data is? Welke HTML moet gerenderd worden als er wel data gePOST is? 
+
+
+👉 Schrijf bij de Wireflows pseudo-code hoe je die state kan bouwen. Kijk in de Liquid documentatie welke tags en filters je zou kunnen gebruiken om een empty state te maken en schrijf ze op het bord
+
+👉 Als je het ontwerp en pseudo-code bedacht hebt, kan je proberen de states te bouwen. Vrijdag gaan we de states testen en/of elkaar helpen met de volgende stap.
+
+💪 Maak de UI states in [partials](https://shopify.github.io/liquid/tags/template/#render)
+
+
+
+### Bronnen
+
+- [UI-Stack - How to fix a bad user interface](https://www.scotthurff.com/posts/why-your-user-interface-is-awkward-youre-ignoring-the-ui-stack/)
+- [Shopify Liquid documentatie](https://shopify.github.io/liquid/)
+- [Officiele Liquid documentatie](https://liquidjs.com/index.html)
--- server.js
+++ server.js
@@ -1,5 +1,249 @@
-console.log('Hier komt je server voor Sprint 10.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from 'express'
 
-console.log('Gebruik uit Sprint 9 alleen de code die je mee wilt nemen.')
+// Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from 'liquidjs';
 
-console.log('Zet \'m op!')
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express()
+
+// Maak werken met data uit formulieren iets prettiger
+app.use(express.urlencoded({ extended: true }))
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static('public'))
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
+
+
+console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
+
+app.locals.opgeslagen = [];
+
+// Maak een GET route voor de index (meestal doe je dit in de root, als /)
+app.get('/', async function (request, response) {
+
+  const params = {
+    fields: '*'
+  }
+
+  const productResponse = await fetch(
+    'https://fdnd-agency.directus.app/items/milledoni_products?' +
+    new URLSearchParams(params)
+  )
+
+  const productResponseJSON = await productResponse.json()
+
+  response.render('index.liquid', {
+    products: productResponseJSON.data
+  })
+
+})
+
+app.get('/lijst', async function (request, response) {
+
+  const userId = 61; // mijn user ID
+  const params = {
+    'filter[milledoni_users_id][_eq]': userId,
+    'fields': '*,milledoni_products_id.*'
+  };
+
+  const productResponse = await fetch('https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1?' +
+    new URLSearchParams(params)
+  )
+  const productResponseJSON = await productResponse.json();
+
+  response.render('lijst.liquid', {
+    products: productResponseJSON.data
+  });
+
+});
+
+// app.get('/lijst', function (request, response) {
+
+//   response.render('lijst.liquid', {
+//     products: app.locals.opgeslagen
+//   })
+
+// });
+
+
+app.post('/opslaan', async function (request, response) {
+
+  const productId = request.body.productId;
+
+  await fetch('https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },//headers= uitleg over je date> Deze data dat ik stuur is json// 
+
+    body: JSON.stringify({ //body=het lichaam van je request
+      milledoni_users_id: 61,
+      milledoni_products_id: productId
+    })
+  });
+
+  response.redirect('/lijst');
+});
+
+
+// app.post('/opslaan', async function (request, response) {
+
+//   const productId = request.body.productId;
+
+//   const productResponse = await fetch(
+//     'https://fdnd-agency.directus.app/items/milledoni_products/' + productId
+//   )
+
+//   const productResponseJSON = await productResponse.json()
+
+//   app.locals.opgeslagen.unshift(productResponseJSON.data) /*unsift is tegenovergestelde van push de nieuw item komt dan als eerst in de array*/
+
+//   response.redirect('/lijst') 
+// })
+app.post('/verwijder', async function (request, response) {
+
+  const id = request.body.id;
+  await fetch(
+    'https://fdnd-agency.directus.app/items/milledoni_users_milledoni_products_1/' + id,
+    {
+      method: 'DELETE'
+    }
+  );
+
+  response.redirect('/lijst');
+
+});
+
+app.get('/valentijnsdag', async function (request, response) {
+
+  const params = {
+    fields: '*',
+    'filter[tags][_contains]': 'valentijnsdag'
+  }
+
+  const productResponse = await fetch(
+    'https://fdnd-agency.directus.app/items/milledoni_products?' +
+    new URLSearchParams(params)
+  )
+
+  const productResponseJSON = await productResponse.json()
+
+  response.render('valentijnsdag.liquid', {
+    products: productResponseJSON.data
+  })
+
+})
+
+app.get('/:tags', async function (request, response) {
+  const params = {
+    fields: 'image,name',
+    'filter[tags][_contains]': request.params.tags
+  }
+
+  const productResponse = await fetch(
+    'https://fdnd-agency.directus.app/items/milledoni_products?' +
+    new URLSearchParams(params)
+  )
+
+  const productResponseJSON = await productResponse.json()
+
+  response.render('index.liquid', {
+    products: productResponseJSON.data
+  })
+
+})
+
+app.get('/product/:id', async function (request, response) {
+
+  const id = request.params.id
+
+  const productResponse = await fetch(
+    'https://fdnd-agency.directus.app/items/milledoni_products/' + id
+  )
+
+  const productResponseJSON = await productResponse.json()
+
+  response.render('product.liquid', {
+    product: productResponseJSON.data
+  })
+
+})
+app.get('/product', async function (request, response) {
+
+  const params = {
+    fields: '*'
+  }
+
+  const productResponse = await fetch(
+    'https://fdnd-agency.directus.app/items/milledoni_products?' +
+    new URLSearchParams(params)
+  )
+
+  const productResponseJSON = await productResponse.json()
+
+  response.render('product.liquid', {
+    products: productResponseJSON.data
+  })
+
+})
+/*
+// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
+app.get(…, async function (request, response) {
+  
+  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
+  response.render(…)
+})
+*/
+
+/*
+// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
+app.post(…, async function (request, response) {
+
+  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
+  console.log(request.body)
+
+  // Via een fetch() naar Directus vullen we nieuwe gegevens in
+
+  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
+  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
+  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
+  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
+  const fetchResponse = await fetch(…, {
+    method: …,
+    body: JSON.stringify(…),
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
+  })
+
+  // Als de POST niet gelukt is, kun je de response loggen. Sowieso een goede debugging strategie.
+  // console.log(fetchResponse)
+
+  // Eventueel kun je de JSON van die response nog debuggen
+  // const fetchResponseJSON = await fetchResponse.json()
+  // console.log(fetchResponseJSON)
+
+  // Redirect de gebruiker daarna naar een logische volgende stap
+  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
+  response.redirect(303, …)
+})
+*/
+
+
+// Stel het poortnummer in waar Express op moet gaan luisteren
+// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+  // Toon een bericht in de console
+  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+})
